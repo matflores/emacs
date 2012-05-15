62c6f9a09be78a3dab10ddbce214b2ff2d3c5cc5
Matías Flores <flores.matias@gmail.com>
2012-05-15 19:41:12 -0300

    Remove unused key bindings

--- bindings.el
+++ bindings.el
@@ -1,10 +1,5 @@
 (provide 'bindings)
 
-(global-set-key (kbd "s-o") 'find-file)         ; open file
-(global-set-key (kbd "s-w") 'close-buffer)      ; close current buffer
-(global-set-key (kbd "s-k") 'kill-this-buffer)  ; close current buffer
-(global-set-key (kbd "C-w") 'kill-word)         ; delete word
-
 (global-set-key [s-up]   'beginning-of-buffer)  ; scroll top
 (global-set-key [s-down] 'end-of-buffer)        ; scroll bottom
 
@@ -20,10 +15,7 @@
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)       ; use ibuffer instead of buffer list
 
-(global-set-key (kbd "C-x C-h") 'bash-term)     ; open terminal buffer
-
 (global-set-key (kbd "C-x g")   'magit-status)  ; magit status
 (global-set-key (kbd "C-x C-g") 'magit-status)  ; magit status
 
-(global-set-key (kbd "C-x c") 'execute-extended-command) ; M-x without meta
 (global-set-key (kbd "C-x m") 'execute-extended-command) ; M-x without meta
